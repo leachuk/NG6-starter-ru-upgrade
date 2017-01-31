83b23e8b75b138f5138404bb8a6abc6315d8a50e
stewart leach <stewartleach@Naya-Las-MacBook-Pro.local>
2017-01-31 22:35:07 +1100

    new router now working

--- client/app/app.js
+++ client/app/app.js
@@ -5,9 +5,9 @@ import 'angular-new-router';
 
 import Common from './common/common';
 import Components from './components/components';
-import config from './app.config';
+//import config from './app.config';
 import AppComponent from './app.component';
-import MainController from './main.controller';
+//import MainController from './main.controller';
 
 
 angular.module('app', [
@@ -15,10 +15,38 @@ angular.module('app', [
     Common,
     Components
   ])
-  .config(config)
   .component('app', AppComponent)
-  .controller('AppController', [MainController])
+  .controller('AppController', ['$router', MainController])
+  .config(['$componentLoaderProvider', '$locationProvider', function($componentLoaderProvider, $locationProvider){
+    console.log('Inline config');
+    console.log($componentLoaderProvider);
 
+    $componentLoaderProvider.setTemplateMapping(function (name) {
+      return 'app/components/' + name + '/' + name + '.html';
+    });
+
+    $locationProvider.html5Mode(true).hashPrefix('!');
+
+    //$locationProvider.html5Mode(true);
+
+    // $mdIconProvider
+    //   .iconSet('action', './assets/svg/action-icons.svg', 24)
+    //   .iconSet('content', './assets/svg/content-icons.svg', 24)
+    //   .iconSet('av', './assets/svg/av-icons.svg', 24)
+    //   .iconSet('navigation', './assets/svg/navigation-icons.svg', 24)
+    //   .iconSet('social', './assets/svg/social-icons.svg', 24)
+    //   .defaultIconSet('./assets/svg/action-icons.svg');
+  }]);
+
+
+function MainController($router){
+  console.log("Inline MainController");
+  console.log($router);
+
+  $router.config([
+    { path: '/home', component: 'home' }
+  ]);
+}
 // class AppController{
 //   constructor() {
 //     console.log("Yahooo!");
--- client/app/main.controller.js
+++ client/app/main.controller.js
@@ -1,12 +1,24 @@
 class MainController {
-  constructor() {
+  constructor($router) {
     "ngInject";
     this.name = 'main bar';
+    this.$router = $router;
+
     console.log('In MainController');
+
+
   }
 
   initApp(){
     console.log("In MainController > initApp");
+    //setup router
+    // this.$router.config([
+    //   { path: '/home', component: 'home' }
+    //   // { path: '/user/:email', component: 'userLanding' },
+    //   // { path: '/user/:email/comparisonrules', component: 'comparisonRule' },
+    //   // { path: '/user/:guid/form', component: 'formSubmit' },
+    //   // { path: '/user/:email/form/:id', component: 'formRead' }
+    // ]);
   }
 }
 
